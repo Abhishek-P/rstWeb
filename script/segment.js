17aa2aa88a64749e57dc9cdcdcc5e3195b33a21a
Abhishek Purushothama <abhishek.purushothama@colorado.edu>
2025-02-14 23:46:30 -0500

    Show segment number by inserting 'segnum' elements The additional element causes problems with savings

--- script/segment.js
+++ script/segment.js
@@ -21,7 +21,7 @@ function act(action){
 
     var action_type = action.split(":")[0];
     var action_params = action.split(":")[1];
-    console.log(action)
+    // console.log(action)
     action_seg = $('#'+action_params).parent().attr("id")
     document.getElementById("logging").value += action + "," + action_seg.replace("seg","");
     if (document.getElementById("undo_state").value == ""){
@@ -33,20 +33,20 @@ function act(action){
     document.getElementById("logging").value += "," + log_undo + ";";
 
 
-    if (action_type =="ins"){
+    if (action_type === "ins"){
         insert_segment(action_params);
         append_undo("del:"+action_params);
         }
-    else if (action_type =="del"){
+    else if (action_type ==="del"){
         delete_segment(action_params);
         append_undo("ins:"+action_params);
         }
-    else if (action_type == "show_seg_num"){
+    else if (action_type === "show_seg_num"){
         show_segment_number(action_params);
     }
-    // else if (action_type == "hide_seg_num"){
-    //     hide_segment_number(action_params);
-    // }
+    else if (action_type === "hide_seg_num"){
+        hide_segment_number(action_params);
+    }
 }
 
 function show_segment_number(segend_id){
@@ -65,38 +65,38 @@ function show_segment_number(segend_id){
         [0, false]
     );
     seg_end = document.getElementById(segend_id)
-    seg_num = seg_num[0]
+    seg_num = seg_num[0] + 2
+    // console.log("seg_num:", seg_num)
     if ( $('#segnum_'+segend_id).length ) {
-        document.getElementById('#segnum_'+segend_id).textContent = seg_num
-        document.getElementById('#segnum_'+segend_id).style.display="inline"
+        seg_num_element = document.getElementById('segnum_'+segend_id)
+        seg_num_element.textContent = seg_num
+        seg_num_element.style.display="inline"
+
+    } else {
+        $('#' + segend_id).append(
+            '<span id="segnum_' + segend_id + '" class="seg_num">' + seg_num + '</span>'
+        )
     }
-    $('#segnum_'+segend_id).after(
-        '<div id="segnum_'+segend_id+' text="'+seg_num+'"/>'
-    )
 }
 
-// function hide_segment_number(segend_id) {
-//     if ( $('#segnum_'+segend_id).length ) {
-//         document.getElementById("#segnum_"+segend_id).style.display="none"
-//     }
-// }
+function hide_segment_number(segend_id) {
+    if ( $('#segnum_'+segend_id).length ) {
+        document.getElementById("segnum_"+segend_id).style.display="none"
+    }
+}
 
 function insert_segment(previous_token_id){
-
     prev_tok = document.getElementById(previous_token_id)
-    if ( $('#segend_post_'+previous_token_id).length ){
-        document.getElementById("segend_post_" + previous_token_id).style.display="inline";
-    }
-    else
-    {
-        $('#'+previous_token_id).after('<div id="segend_post_'+previous_token_id+'" class="seg_end" onclick="act('+"'del:"+previous_token_id+"'"+')">||<div/>');
+    segend_id = 'segend_post_'+previous_token_id
+    if ( $('#'+segend_id).length ){
+        document.getElementById(segend_id).style.display="inline";
+    } else {
+        $('#'+previous_token_id).after('<div id="'+segend_id+'" class="seg_end" onclick="act('+"'del:"+previous_token_id+"'"+')" onmouseover="act('+"'show_seg_num:"+segend_id+"'"+')" onmouseout="act('+"'hide_seg_num:"+segend_id+"')" + '">||</div>');
     }
     document.getElementById(previous_token_id).style.display = "none";
 }
 
 function delete_segment(seg_last_tok_id){
-
     document.getElementById("segend_post_"+seg_last_tok_id).style.display = "none";
     document.getElementById(seg_last_tok_id).style.display="inline";
-
 }
